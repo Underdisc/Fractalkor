4ea26e85c239f36b7c6c054f75375dde383c9bee
Underdisc <connortdeakin@gmail.com>
2021-03-10 12:05:02 +0000

    Clang formatted and cleaned up the code inside of Main.cc.

--- src/Main.cc
+++ src/Main.cc
@@ -1,28 +1,39 @@
-#include <iostream>
+// glad.h must be included before Input.h because Input.h includes glfw3.h.
+// Really, these two files should never need to be included in the same file,
+// but since this is just a small experiment project, this is being overlooked.
+// clang-fromat off
+#include <Input.h>
+#include <glad/glad.h>
+// clang-format on
 
 #include <Error.h>
+#include <VarkorMain.h>
 #include <Viewport.h>
 #include <gfx/Shader.h>
-#include <glad/glad.h>
-#include <Input.h>
+#include <imgui/imgui.h>
 #include <math/Vector.h>
 #include <world/World.h>
-#include <VarkorMain.h>
-#include <imgui/imgui.h>
 
+// The height and location of the area being rendered.
 float gHeight = 2.0f;
 Vec2 gCenter = {0.0f, 0.0f};
+// The number of times the fractal function is executed in the shader
 int gIterations = 10;
+// C's value in f(z) = z^2 + c. This constant is only used by the Julia shader.
 Vec2 gC = {0.0f, 0.0f};
-Gfx::Shader gMandelbrotShader;
-Gfx::Shader gJuliaShader;
 unsigned int gVao;
 
+// The shaders and some values to keep track of the currently selected shader.
+Gfx::Shader gMandelbrotShader;
+Gfx::Shader gJuliaShader;
 const int gMandelbrotIndex = 0;
 const int gJuliaIndex = 1;
 int gSelectedShader;
 
-void PostRender()
+// CentralUpdate really shouldn't contain editor code and rendering code, but
+// this is a little project that ignores an organized approach because it's not
+// large enough to warrant it.
+void CentralUpdate()
 {
   // Display the imgui window for modifying the fractal image.
   ImGui::Begin("Fractal Controls");
@@ -34,8 +45,7 @@ void PostRender()
   }
   ImGui::End();
 
-  // Move the center using the mouse input and zoom in and out using the scroll
-  // wheel.
+  // Move the center using the mouse input and zoom using the scroll wheel.
   if (Input::MouseDown(Input::Mouse::Left))
   {
     gCenter[0] -= Input::MouseMotion()[0] * gHeight * 0.001f;
@@ -51,8 +61,7 @@ void PostRender()
   {
     gJuliaShader.SetVec2("uC", gC.CData());
     activeShader = &gJuliaShader;
-  }
-  else
+  } else
   {
     activeShader = &gMandelbrotShader;
   }
@@ -70,15 +79,16 @@ void PostRender()
 
 int main(void)
 {
+  // Initialize Varkor.
   VarkorInit();
-  World::PostRender = PostRender;
+  World::CentralUpdate = CentralUpdate;
 
   // Initialize the shaders.
-  Gfx::Shader::InitResult initResult = gMandelbrotShader.Init(
-      "res/shader/Fractal.vs", "res/shader/Mandelbrot.fs");
+  Gfx::Shader::InitResult initResult =
+    gMandelbrotShader.Init("res/shader/Fractal.vs", "res/shader/Mandelbrot.fs");
   LogAbortIf(!initResult.mSuccess, initResult.mError.c_str());
   initResult =
-      gJuliaShader.Init("res/shader/Fractal.vs", "res/shader/Julia.fs");
+    gJuliaShader.Init("res/shader/Fractal.vs", "res/shader/Julia.fs");
   LogAbortIf(!initResult.mSuccess, initResult.mError.c_str());
   gSelectedShader = 0;
 
@@ -101,6 +111,9 @@ int main(void)
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
   glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  VarkorMain();
+  // Run and purge Varkor.
+  VarkorEngine();
+  VarkorPurge();
 }
